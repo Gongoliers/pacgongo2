388bf7446c611f9d401e4b1e938a3c81820153ec
kylecorry31 <kylecorry31@gmail.com>
2015-08-31 17:18:32 -0400

    Changed map design; Implemented wall sensors; A* algorithm for ghosts

--- game.js
+++ game.js
@@ -2,199 +2,399 @@
  * Author - Kyle Corry
  */
 
-var c = document.getElementById("game");
-var ctx = c.getContext("2d");
-var white = "#ffffff";
-var black = "#000000";
-var yellow = "#ffff00";
-var blue = "#0000ff";
-var blockSize = 25;
-var height = ctx.canvas.height;
-var width = ctx.canvas.width;
-var jump = 8;
-var ghostSpeed = 5;
-var fps = 16;
-var score = 0;
-var lives = 3;
+var WIDTH = 500,
+  HEIGHT = 500;
+var canvas, ctx, keystate;
+var Pacgongo, ghosts, Ghost, player;
+var numGhosts = 4;
+var up = 38,
+  down = 40,
+  left = 37,
+  right = 39;
+var BLOCK = 25;
+var total_points = 222;
+var clear_points = 50;
+var round = 0;
+var running = true;
 
-// Pacgongo position
-var x = width / 2 - blockSize / 2;
-var y = height - blockSize;
+/*
+Left 2
+Bottom 4
+Right 8
+Top 16
 
-var dx = 0;
-var dy = 0;
+*/
 
-// Images
-var pacgongo = document.getElementById("pacgongo");
-var ghostcoffey = document.getElementById("ghostcoffey");
-
-
-var ghostXs = [
-  100, 120, 10, 50
+var map = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+  [0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0],
+  [0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0],
+  [0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0],
+  [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+  [0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0],
+  [0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0],
+  [0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0],
+  [0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0],
+  [0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0],
+  [0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0],
+  [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+  [0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0],
+  [0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0],
+  [0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+  [0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0],
+  [0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0],
+  [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
 
-var ghostYs = [
-  130, 20, 15, 90
-];
+var backup_map = clone(map);
 
-var riding = false;
-var gameOver = false;
-var gamePaused = false;
-var intervalId = null;
+Pacgongo = function() {
+  return {
+    image: document.getElementById("pacgongo"),
+    x: 0,
+    y: 0,
+    vel: {
+      x: 0,
+      y: 0
+    },
+    score: 1,
+    width: BLOCK,
+    height: BLOCK,
+    lives: 3,
+    speed: 3,
+    wallSensor: {
+      front: null,
+      left: null,
+      right: null,
+      back: null
+    },
+    sensorUpdate: function() {
+      var normalX = Math.round(this.x / BLOCK);
+      var normalY = Math.round(this.y / BLOCK);
 
+      if (map[normalY][Math.round(this.x / BLOCK - 0.5)] === 0) {
+        this.wallSensor.left = 1;
+      } else {
+        this.wallSensor.left = 0;
+      }
 
-var gameLoop = function() {
-  document.getElementById('soundtrack').play();
-  ctx.font = "25px SanSerif";
-  ctx.clearRect(0, 0, width, height);
-  ctx.fillStyle = white;
-  ctx.fillRect(0, 0, width, height);
-  ctx.fillStyle = black;
-  if (gameOver) {
-    clearInterval(intervalId);
-    intervalId = setInterval(gameEnd, 1000 / fps);
+      if (map[normalY][Math.round(this.x / BLOCK + 0.25)] === 0) {
+        this.wallSensor.right = 1;
+      } else {
+        this.wallSensor.right = 0;
+      }
 
-  }
-  if (gamePaused) {
-    clearInterval(intervalId);
-    intervalId = setInterval(gamePause, 1000 / fps);
-  }
+      if (map[Math.round(this.y / BLOCK + 0.5)][normalX] === 0) {
+        this.wallSensor.back = 1;
+      } else {
+        this.wallSensor.back = 0;
+      }
 
-  drawBackground();
-  drawObjects();
-  window.onkeydown = function(e) {
-    var key = e.keyCode ? e.keyCode : e.which;
-    if (key == 38) {
-      dy = -jump;
-      dx = 0;
-    } else if (key == 40) {
-      dy = jump;
-      dx = 0;
-    } else if (key == 39) {
-      dx = jump;
-      dy = 0;
-    } else if (key == 37) {
-      dx = -jump;
-      dy = 0;
-    } else if (key == 27) {
-      gameOver = true;
-    } else if (key == 80) {
-      gamePaused = true;
+      if (map[Math.round(this.y / BLOCK - 0.5)][normalX] === 0) {
+        this.wallSensor.front = 1;
+      } else {
+        this.wallSensor.front = 0;
+      }
+
+    },
+    navMap: function() {
+      this.sensorUpdate();
+      var i = Math.round(this.x / BLOCK);
+      var j = Math.round(this.y / BLOCK);
+      if ((map[j][i] === 1)) {
+        new Audio("res/pacman-waka-waka.mp3").play();
+        this.score++;
+        map[j][i]++;
+      }
+      if (this.wallSensor.left === 1 && this.vel.x < 0) {
+        this.vel.x = 0;
+      }
+
+      if (this.wallSensor.right === 1 && this.vel.x > 0) {
+        this.vel.x = 0;
+      }
+
+      if (this.wallSensor.front === 1 && this.vel.y < 0) {
+        this.vel.y = 0;
+      }
+
+      if (this.wallSensor.back === 1 && this.vel.y > 0) {
+        this.vel.y = 0;
+      }
+    },
+    update: function() {
+      if (keystate == up) {
+        this.vel.y = -this.speed;
+        this.vel.x = 0;
+      } else if (keystate == down) {
+        this.vel.y = this.speed;
+        this.vel.x = 0;
+      } else if (keystate == left) {
+        this.vel.y = 0;
+        this.vel.x = -this.speed;
+      } else if (keystate == right) {
+        this.vel.y = 0;
+        this.vel.x = this.speed;
+      }
+      this.navMap();
+      this.x += this.vel.x;
+      this.y += this.vel.y;
+
+    },
+    draw: function() {
+      this.image.height = this.height;
+      this.image.width = this.width;
+      ctx.drawImage(this.image, this.x, this.y);
+      document.getElementById("lives").innerHTML = this.lives;
+      document.getElementById("score").innerHTML = 'Score: ' + this.score;
+      document.getElementById("round").innerHTML = 'Round: ' + round;
     }
-
   };
-  wallCollision();
 };
 
-var drawBackground = function() {
+Ghost = function() {
+  return {
+    image: document.getElementById("ghostcoffey"),
+    x: 0,
+    y: 0,
+    vel: {
+      x: 0,
+      y: 0
+    },
+    wallSensor: {
+      front: null,
+      left: null,
+      right: null,
+      back: null
+    },
+    sensorUpdate: function() {
+      var normalX = Math.round(this.x / BLOCK);
+      var normalY = Math.round(this.y / BLOCK);
 
+      if (map[normalY][Math.round(this.x / BLOCK - 0.5)] === 0) {
+        this.wallSensor.left = 1;
+      } else {
+        this.wallSensor.left = 0;
+      }
+
+      if (map[normalY][Math.round(this.x / BLOCK + 0.25)] === 0) {
+        this.wallSensor.right = 1;
+      } else {
+        this.wallSensor.right = 0;
+      }
+
+      if (map[Math.round(this.y / BLOCK + 0.5)][normalX] === 0) {
+        this.wallSensor.back = 1;
+      } else {
+        this.wallSensor.back = 0;
+      }
+
+      if (map[Math.round(this.y / BLOCK - 0.5)][normalX] === 0) {
+        this.wallSensor.front = 1;
+      } else {
+        this.wallSensor.front = 0;
+      }
+
+    },
+    width: BLOCK,
+    height: BLOCK,
+    speed: 2,
+    navMap: function() {
+      this.sensorUpdate();
+      var i = Math.round(this.x / BLOCK);
+      var j = Math.round(this.y / BLOCK);
+
+      if (this.wallSensor.left === 1 && this.vel.x < 0) {
+        this.vel.x = 0;
+      }
+
+      if (this.wallSensor.right === 1 && this.vel.x > 0) {
+        this.vel.x = 0;
+      }
+
+      if (this.wallSensor.front === 1 && this.vel.y < 0) {
+        this.vel.y = 0;
+      }
+
+      if (this.wallSensor.back === 1 && this.vel.y > 0) {
+        this.vel.y = 0;
+      }
+
+    },
+    moveToPacgongo: function() {
+      var grid = getAstarMap();
+      var finder = new PF.AStarFinder();
+      var path = finder.findPath(Math.round(this.x / BLOCK), Math.round(this.y / BLOCK),
+        Math.round(player.x / BLOCK), Math.round(player.y / BLOCK), grid);
+      if (path[1]) {
+        if (path[1][0] * BLOCK < this.x) {
+          this.vel.x = -this.speed;
+        } else if (path[1][0] * BLOCK > this.x) {
+          this.vel.x = this.speed;
+        } else {
+          this.vel.x = 0;
+        }
+
+        if (path[1][1] * BLOCK < this.y) {
+          this.vel.y = -this.speed;
+        } else if (path[1][1] * BLOCK > this.y) {
+          this.vel.y = this.speed;
+        } else {
+          this.vel.y = 0;
+        }
+
+      }
+
+
+    },
+    update: function() {
+      this.moveToPacgongo();
+      this.navMap();
+      this.x += this.vel.x;
+      this.y += this.vel.y;
+      if (intersect(this.x, this.y, this.width, this.height,
+          player.x, player.y, player.width, player.height)) {
+        player.lives--;
+        resetField();
+      }
+    },
+    draw: function() {
+      this.image.height = this.height;
+      this.image.width = this.width;
+      ctx.drawImage(this.image, this.x, this.y);
+    }
+  };
 };
 
-var drawObjects = function() {
-
-  movePacgongo();
-
-  moveGhosts();
-
-  // Pacgongo
-  ctx.drawImage(pacgongo, x, y);
-
-  // ghostcoffey
-  for (var i = 0; i < ghostXs.length; i++) {
-    ctx.drawImage(ghostcoffey, ghostXs[i], ghostYs[i]);
-  }
-
-  if (colliding() && !gameOver) {
-    lives--;
-    x = width / 2 - blockSize / 2;
-    y = height - blockSize;
-    dx = 0;
-    dy = 0;
-    ghostXs = [
-      100, 120, 10, 50
-    ];
-
-    ghostYs = [
-      130, 20, 15, 90
-    ];
-    if (lives <= 0) {
-      gameOver = true;
+function resetField() {
+  if (running) {
+    player.x = (WIDTH) / 2;
+    player.y = HEIGHT - player.height - BLOCK;
+    player.vel = {
+      x: 0,
+      y: 0
+    };
+    keystate = 0;
+    ghosts = [];
+    for (var i = 0; i < numGhosts; i++) {
+      var tempGhost = new Ghost();
+      tempGhost.x = (Math.random() * (6 * BLOCK - tempGhost.width)) + 8 * BLOCK;
+      tempGhost.y = Math.random() * (2 * BLOCK - tempGhost.height) + 7 * BLOCK;
+      ghosts.push(tempGhost);
     }
   }
+}
 
-  $('#lives').empty();
-  for(var j = 0; j < lives; j++){
-    $('#lives').append('<img src="res/pacgongo.png"> ');
-  }
-  $('#score').text(score + " points");
-};
-
-
-var moveGhosts = function() {
-  for (var i = 0; i < ghostXs.length; i++) {
-    ghostXs[i] += x > ghostXs[i] ? ghostSpeed : -ghostSpeed;
-    ghostYs[i] += y > ghostYs[i] ? ghostSpeed : -ghostSpeed;
-  }
-};
-
-
-var movePacgongo = function() {
-  y += dy;
-  x += dx;
-};
-
-var colliding = function() {
-  for (var i = 0; i < ghostXs.length; i++) {
-    if (ghostXs[i] < x + pacgongo.width && ghostXs[i] + ghostcoffey.width > x) {
-      if (ghostYs[i] < y + pacgongo.height && ghostYs[i] + ghostcoffey.height > y) {
-        return true;
+function drawMap() {
+  ctx.fillStyle = 'darkgreen';
+  for (var i = 0; i < map.length; i++) {
+    for (var j = 0; j < map[i].length; j++) {
+      if (map[i][j] === 1) {
+        ctx.fillRect(j * BLOCK + BLOCK / 2, i * BLOCK + BLOCK / 2, 4, 4);
+      } else if (map[i][j] === 0) {
+        ctx.fillRect(j * BLOCK, i * BLOCK, BLOCK, BLOCK);
       }
     }
   }
-  return false;
-};
+}
 
-var wallCollision = function() {
-  if (x < 0) {
-    x = 0;
-  } else if (x > width - blockSize) {
-    x = width - blockSize;
-  } else if (y < jump) {
-    y = 0;
-  } else if (y > height - blockSize) {
-    y = height - blockSize;
+
+function intersect(ax, ay, aw, ah, bx, by, bw, bh) {
+  return ax < bx + bw && ay < by + bh && bx < ax + aw && by < ay + ah;
+}
+
+function main() {
+  canvas = document.getElementById("game");
+  ctx = canvas.getContext("2d");
+  backup_map = map.slice();
+  document.addEventListener("keydown", function(evt) {
+    keystate = evt.keyCode;
+  });
+
+  init();
+
+  var loop = function() {
+    update();
+    draw();
+    window.requestAnimationFrame(loop, canvas);
+  };
+  window.requestAnimationFrame(loop, canvas);
+}
+
+function init() {
+  player = new Pacgongo();
+  resetField();
+  var music = new Audio("res/pacman.mp3");
+  music.loop = true;
+  music.play();
+
+}
+
+function clone(src) {
+  var dest = [];
+  for (var i = 0; i < src.length; i++) {
+    var arr = [];
+    for (var j = 0; j < src[i].length; j++) {
+      arr.push(src[i][j]);
+    }
+    dest.push(arr);
   }
-};
+  return dest;
+}
 
-var gamePause = function() {
-  ctx.clearRect(0, 0, width, height);
-  ctx.fillStyle = white;
-  ctx.fillRect(0, 0, width, height);
-  ctx.fillStyle = black;
-  ctx.fillText("Game Paused, press P to resume", width / 2 - "Game Paused, press P to resume".length * 5, height / 3);
-  window.onkeydown = function(e) {
-    var key = e.keyCode ? e.keyCode : e.which;
-    if (key == 80) {
-      gamePaused = false;
-      clearInterval(intervalId);
-      intervalId = setInterval(gameLoop, 1000 / fps);
+function getAstarMap() {
+  var grid = new PF.Grid(map[0].length, map.length);
+  for (var i = 0; i < map.length; i++) {
+    for (var j = 0; j < map[i].length; j++) {
+      if (map[i][j] === 2 || map[i][j] === 1) {
+        grid.setWalkableAt(j, i, true);
+      } else {
+        grid.setWalkableAt(j, i, false);
+      }
     }
-  };
-};
+  }
+  return grid;
+}
 
-var gameEnd = function() {
-  ctx.clearRect(0, 0, width, height);
-  ctx.fillStyle = white;
-  ctx.fillRect(0, 0, width, height);
-  ctx.fillStyle = black;
-  ctx.fillText("Game Over, press C to play again", width / 2 - "Game Over, press C to play again".length * 5, height / 3);
-  window.onkeydown = function(e) {
-    var key = e.keyCode ? e.keyCode : e.which;
-    if (key == 67) {
-      clearInterval(intervalId);
-      window.location.reload();
+function update() {
+  // document.getElementById('soundtrack').play();
+  if (!running) {
+    player.y += player.speed * 3;
+    player.draw();
+  } else {
+    if (player.lives <= 0) {
+      running = false;
+      // alert('Game Over ---- You scored ' + player.score + ' points!');
+      new Audio("res/pacman-dying.mp3").play();
+
+      setTimeout(function() {
+        location.reload();
+      }, 2000);
+
     }
-  };
-};
+    if (((player.score - clear_points * round) % total_points) === 0) {
+      round++;
+      map = clone(backup_map);
+      resetField();
+    }
+    player.update();
+    for (var i = 0; i < ghosts.length; i++) {
+      ghosts[i].update();
+    }
+  }
+}
 
-var intervalId = setInterval(gameLoop, 1000 / fps);
+function draw() {
+  ctx.fillStyle = '#fff';
+  ctx.fillRect(0, 0, WIDTH, HEIGHT);
+  drawMap();
+  player.draw();
+  for (var i = 0; i < ghosts.length; i++) {
+    ghosts[i].draw();
+  }
+}
+
+
+main();
